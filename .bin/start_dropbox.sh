68203445b1c88eba5efa902015cd2a3ef0c1c058
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2011-11-10 19:53:19 +0300

    Added script name to messages from Dropbox script

--- .bin/start_dropbox.sh
+++ .bin/start_dropbox.sh
@@ -4,14 +4,14 @@ PSLIST=`ps -ef`
 echo "$PSLIST" | grep -v 'start_dropbox.sh' | grep -q 'dropbox'
 if [ "$?" -ne "0" ]; then
     if [ ! "$DISPLAY" == "" ]; then
-	echo "Starting dropbox service..."
+	echo "`basename $0`: Starting dropbox service..."
 	nohup dropbox 1>/dev/null 2>&1 &
 	rm -f nohup.out
     else
-	echo "X server not running!"
+	echo "`basename $0`: X server not running!"
 	exit 1
     fi
 else
-    echo "Dropbox already started!"
+    echo "`basename $0`: Dropbox already started!"
     exit 1
 fi
