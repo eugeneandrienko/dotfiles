5f8fc8e620e514625f6540aef6a67ffbc8ea0046
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2012-06-17 17:12:14 +0400

    Now I can use use right and left second display, not only right

--- .bin/displays.sh
+++ .bin/displays.sh
@@ -17,7 +17,8 @@ TEMPFILE=`mktemp /tmp/displays.XXXXXX`
 
 dialog --menu "Select mode" 40 80 30 \
 "1m" "One main display (1280x800)" \
-"2m1" "Main display (1280x800) and second (1024x768)" \
+"2m1r" "Main display (1280x800) and second (1024x768) right" \
+"2m1l" "Main display (1280x800) and second (1024x768) left" \
 "2m2" "Main display (1280x800) and second (1280x960)" \
 2>"$TEMPFILE"
 
@@ -31,10 +32,15 @@ case "$SELITEM" in
         xrandr --fb 1280x800 --output LVDS1 --primary --mode 1280x800 \
 --pos 0x0 --output VGA1 --off
         ;;
-    "2m1")
+    "2m1r")
         check_connection "VGA1"
 	xrandr --fb 2304x800 --output LVDS1 --primary --mode 1280x800 \
 --pos 0x0 --output VGA1 --mode 1024x768 --pos 1280x0
+	;;
+    "2m1l")
+	check_connection "VGA1"
+	xrandr --fb 2304x800 --output VGA1 --mode 1024x768 \
+--pos 0x0 --output LVDS1 --primary --mode 1280x800 --pos 1024x0
 	;;
     "2m2")
 	check_connection "VGA1"
