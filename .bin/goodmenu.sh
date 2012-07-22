7b2a698409166b1314202c6638088e32bfd05fe2
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2012-07-22 15:19:10 +0400

    Added new items to my goodmenu

--- .bin/goodmenu.sh
+++ .bin/goodmenu.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-google-chrome &
+EXECLIST="google-chrome\n\
+emacs\n\
+skype\n\
+gimp\n\
+audacity\n\
+lock"
+ECHO="/bin/echo -e"
+DMENU='dmenu -b -p "Execute:" -nb black -nf green -sb green -sf black'
+RETVAL=`$ECHO $EXECLIST | $DMENU -fn "-*-droid sans mono-*-*-*-*-12-*-*-*-*-*-*-u"`
+
+case "$RETVAL" in
+    "google-chrome")
+	google-chrome &
+	;;
+    "emacs")
+	emacsclient -c -n &
+	;;
+    "skype")
+	skype &
+	;;
+    "gimp")
+	gimp &
+	;;
+    "audacity")
+	audacity & 
+	;;
+    "lock")
+	/home/drag0n/.bin/xlock.sh
+	;;
+esac
+
+exit 0
