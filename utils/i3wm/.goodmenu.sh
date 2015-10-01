9068e108c560e60095a0f7b2e635c69d6bf8afda
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-10-01 12:28:18 +0300

    Add configuration files for i3wm and urxvt

---
+++ utils/i3wm/.goodmenu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+FONT='-xos4-terminus-*-*-*-*-14-*-*-*-*-*-*-u'
+COLORS='-sb #00dd00 -sf #000000 -nb #000000 -nf #00dd00'
+DMENU_CMD="dmenu -p 'Execute:' -l 5 -i -b -fn $FONT $COLORS"
+DMENU_ITEMS="chrome:audacity:gimp:krita:enable touchpad:disable touchpad"
+
+case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
+    'chrome')
+        google-chrome-stable &
+        ;;
+    'audacity')
+        audacity &
+        ;;
+    'gimp')
+        gimp &
+        ;;
+    'krita')
+        krita &
+        ;;
+    'enable touchpad')
+        synclient TouchpadOff=0
+        ;;
+    'disable touchpad')
+        synclient TouchpadOff=1
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
