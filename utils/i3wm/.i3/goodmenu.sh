407f30f70aa951cd296ab2afa2f8eabfc1bc33dc
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2019-03-06 22:11:22 +0300

    Add some new program to dmenu

--- utils/i3wm/.i3/goodmenu.sh
+++ utils/i3wm/.i3/goodmenu.sh
@@ -3,7 +3,7 @@
 FONT='-xos4-terminus-*-*-*-*-14-*-*-*-*-*-*-u'
 COLORS='-sb #000000 -sf #ffffff -nb #ffffff -nf #000000'
 DMENU_CMD="dmenu -p Execute: -l 5 -i -b -fn $FONT $COLORS"
-DMENU_ITEMS="firefox:firefox_unsec:telegram:keepassxc:qbittorrent:guitarpro"
+DMENU_ITEMS="firefox:firefox_unsec:telegram:keepassxc:qbittorrent:idea:guitarpro:krita:simple_scan:android-file-transfer"
 
 case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
     'firefox')
@@ -21,8 +21,20 @@ case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
     'qbittorrent')
         /usr/bin/qbittorrent &
         ;;
+    'idea')
+        /home/drag0n/idea/bin/idea.sh
+        ;;
     'guitarpro')
         wine /home/drag0n/.wine/drive_c/Program\ Files\ \(x86\)/Arobas\ Music/Guitar\ Pro\ 7/GuitarPro7.exe &
+        ;;
+    'krita')
+        /usr/bin/krita &
+        ;;
+    'simple_scan')
+        /usr/bin/simple-scan &
+        ;;
+    'android-file-transfer')
+        /usr/bin/android-file-transfer &
         ;;
     *)
         exit 1
