2222f6b612d44f4343966b64ecab1bcd15c6dfa6
eugeneandrienko <evg.andrienko@gmail.com>
2020-06-23 14:50:17 +0300

    Add mark manipulation scripts

---
+++ utils/i3wm/.i3/set_mark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FONT='-xos4-terminus-*-*-*-*-14-*-*-*-*-*-*-u'
+COLORS='-sb #000000 -sf #ffffff -nb #ffffff -nf #000000'
+DMENU_CMD="dmenu -i -b -fn $FONT $COLORS"
+
+LIST_OF_MARKS='ff:fp:k:t'
+MARK="$(echo $LIST_OF_MARKS | tr ':' '\n' | $DMENU_CMD -p 'Set mark:')"
+if [ -n "$MARK" ]; then
+    i3-msg "mark --toggle $MARK"
+fi
+
