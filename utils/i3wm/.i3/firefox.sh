1e56c94507e7dffdb3006685a6a8eaf82cadcaa7
eugeneandrienko <evg.andrienko@gmail.com>
2020-08-23 00:18:37 +0300

    Reformat firefox launcher

--- utils/i3wm/.i3/firefox.sh
+++ utils/i3wm/.i3/firefox.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Remove nogroups from /etc/firejail/firefox-common.profile if no sound in Firefox
-if [ -x /usr/bin/firejail ] && [ -x /usr/bin/apulse ] && [ -x /usr/bin/firefox-bin ] && [ -r /etc/firejail/firefox.profile ]; then
-    firejail --profile=/etc/firejail/firefox.profile --whitelist=~/downloads/ /usr/bin/apulse /usr/bin/firefox-bin &
+if [ -x /usr/bin/firejail ] &&
+    [ -x /usr/bin/apulse ] &&
+    [ -x /usr/bin/firefox-bin ] &&
+    [ -r /etc/firejail/firefox.profile ]; then
+    firejail --profile=/etc/firejail/firefox.profile \
+    --whitelist=~/downloads/ \
+    /usr/bin/apulse /usr/bin/firefox-bin &
 fi
 
