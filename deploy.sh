746b8eef90a804064789b2bdef7f24fd547fb45c
eugeneandrienko <evg.andrienko@gmail.com>
2020-09-01 23:13:07 +0300

    Use shell-script instead of python file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 
 # Settings:
 GITREPO_NAME="dotfiles"
-CONFIG_DIRECTORIES_FILE="config_directories.txt"
+CONFIG_DIRECTORIES="config_directories.txt"
+PROTECTED_FILES="protected_files.txt"
 
 
 # Are we in the directory with cloned dotfiles repository?
@@ -12,10 +13,27 @@ if [ "$(basename "$PWD")" != "$GITREPO_NAME" ] || [ ! -d ".git/" ]; then
     exit 1
 fi
 
+# Are necessary configuration files exists?
+if [ ! -e "$CONFIG_DIRECTORIES" ] || [ ! -e "$PROTECTED_FILES" ]; then
+    echo "Configuration files: $CONFIG_DIRECTORIES, $PROTECTED_FILES - does not exists!"
+    exit 2
+fi
 
 # Copying dotfiles to $HOME
-for directory in $(cat "$CONFIG_DIRECTORIES_FILE"); do
-    cp -Rv $directory/ ~
+for directory in $(cat "$CONFIG_DIRECTORIES"); do
+    for from_file in $(find "$directory" -type f); do
+        to_file=$(echo "$from_file" | sed -r "s!$directory/(.*)!\1!g")
+        if [ -e "$HOME/$to_file" ] && grep -q "$to_file" "$PROTECTED_FILES"; then
+            echo "File $to_file already exists in $HOME and protected!"
+            echo
+            diff "$from_file" "$HOME/$to_file"
+            echo
+            echo "Skipping $from_file"
+            echo
+        else
+            cp -v "$from_file" "$HOME/$to_file"
+        fi
+    done
 done
 
 
@@ -29,11 +47,6 @@ mkdir -pv "$HOME/.mail/logs"
 chmod -v 600 ~/.fetchmailrc
 chmod -v 600 ~/.msmtprc
 
-# Warn user what he need to change some files:
-echo "Place valid data instead of placeholders in next files:"
-echo "~/.fetchmailrc"
-echo "~/.msmtprc"
-
 echo
-echo "Almost done!"
+echo "Done!"
 
