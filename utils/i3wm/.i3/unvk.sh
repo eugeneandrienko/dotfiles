09e21d3424c9c7fe5c896b21c7f5e15acd94e819
eugeneandrienko <evg.andrienko@gmail.com>
2020-09-04 14:00:21 +0300

    Fix '&cc_key=' ending on the unvk.sh script

--- utils/i3wm/.i3/unvk.sh
+++ utils/i3wm/.i3/unvk.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-xclip -out | sed -r 's/https:\/\/vk\.com\/away\.php\?to=(.*)/\1/g' | python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));" | xclip -in
+xclip -out | sed -r 's/https:\/\/vk\.com\/away\.php\?to=(.*)$/\1/g' | sed -r 's/(.*)&cc_key=/\1/g' | python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));" | xclip -in
 
