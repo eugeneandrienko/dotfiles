39f2bd05ae322c161d885f3aef3334ec54e2fb28
eugeneandrienko <evg.andrienko@gmail.com>
2020-09-08 23:47:19 +0300

    Fix i3wm scripts for FreeBSD

--- utils/i3wm/.i3/unvk.sh
+++ utils/i3wm/.i3/unvk.sh
@@ -5,3 +5,4 @@ xclip -out | \
     sed -r 's/(.*)&cc_key=/\1/g' | \
     sed -r 's/(.*)&post=[-0-9_]+/\1/g' | \
     python3.7 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));" | xclip -in
+
