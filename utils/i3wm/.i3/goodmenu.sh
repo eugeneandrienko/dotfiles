ac2e07cd00e49ca6f1223fa0d26e8c84db53a759
eugeneandrienko <evg.andrienko@gmail.com>
2020-09-08 23:47:19 +0300

    Fix i3wm scripts for FreeBSD

--- utils/i3wm/.i3/goodmenu.sh
+++ utils/i3wm/.i3/goodmenu.sh
@@ -5,7 +5,6 @@ COLORS='-sb #000000 -sf #ffffff -nb #ffffff -nf #000000'
 DMENU_CMD="dmenu -p Execute: -i -b -fn $FONT $COLORS"
 
 DMENU_ITEMS="firefox:firefox_unsec:telegram:keepassxc"
-DMENU_ITEMS+=":audacious:idea:gimp:openshot"
 DMENU_ITEMS+=":android-file-transfer:qmapshack"
 
 case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
@@ -13,31 +12,19 @@ case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
         /home/drag0n/.i3/firefox.sh
         ;;
     'firefox_unsec')
-        /usr/bin/apulse /usr/bin/firefox-bin -P unsecure &
+        /usr/local/bin/firefox -P unsecure &
         ;;
     'telegram')
         /home/drag0n/telegram/Telegram &
         ;;
     'keepassxc')
-        /usr/bin/keepassxc &
-        ;;
-    'audacious')
-        /usr/bin/audacious &
-        ;;
-    'idea')
-        /home/drag0n/idea/bin/idea.sh
-        ;;
-    'gimp')
-        /usr/bin/gimp &
-        ;;
-    'openshot')
-        /usr/bin/openshot-qt &
+        /usr/local/bin/keepassxc &
         ;;
     'android-file-transfer')
-        /usr/bin/android-file-transfer &
+        /usr/local/bin/android-file-transfer &
         ;;
     'qmapshack')
-         LC_MESSAGES="ru_RU.utf8" /usr/bin/qmapshack &
+         LC_MESSAGES="ru_RU.utf8" /usr/local/bin/qmapshack &
         ;;
     *)
         exit 1
