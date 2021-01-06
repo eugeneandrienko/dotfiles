0ba2b500bcb18848414936782c38055deee001f9
eugeneandrienko <evg.andrienko@gmail.com>
2021-01-06 19:03:47 +0300

    Add propagate_changes.sh script.
    Add script to propagate given commits to other branches.

---
+++ propagate_changes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ "$#" -eq "0" ]; then
+    echo "No one commit hash given!"
+    exit 1
+fi
+
+CURRENT_BRANCH=$(git branch --no-color --show-current)
+BRANCHES=$(git branch --format='%(refname:short)' | grep -v "$CURRENT_BRANCH")
+
+for branch in $BRANCHES; do
+    git checkout "$branch"
+    git cherry-pick "$@"
+    git push origin
+    git push e
+done
+
+git checkout "$CURRENT_BRANCH"
+
