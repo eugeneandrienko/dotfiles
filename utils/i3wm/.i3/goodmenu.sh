1c33bd76020c76b9abac8f5ddc7ae3874fedea75
eugeneandrienko <evg.andrienko@gmail.com>
2021-03-21 22:57:50 +0300

    Revert "Add audacity to i3 menu"
    This reverts commit 640e13cf1be3493289e51589982f508e545e73f8.

--- utils/i3wm/.i3/goodmenu.sh
+++ utils/i3wm/.i3/goodmenu.sh
@@ -6,7 +6,6 @@ DMENU_CMD="dmenu -p Execute: -i -b -fn $FONT $COLORS"
 
 DMENU_ITEMS="firefox:firefox_unsec:telegram:keepassxc"
 DMENU_ITEMS+=":android-file-transfer:qmapshack"
-DMENU_ITEMS+=":audacity"
 
 case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
     'firefox')
@@ -26,9 +25,6 @@ case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
         ;;
     'qmapshack')
          LC_MESSAGES="ru_RU.utf8" /usr/local/bin/qmapshack &
-        ;;
-    'audacity')
-        /usr/bin/audacity &
         ;;
     *)
         exit 1
