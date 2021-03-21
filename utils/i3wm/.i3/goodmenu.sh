c026692a5160e9320d49131dc77acc04e1fd659d
eugeneandrienko <evg.andrienko@gmail.com>
2021-03-21 22:53:08 +0300

    Add JOSM to i3 menu

--- utils/i3wm/.i3/goodmenu.sh
+++ utils/i3wm/.i3/goodmenu.sh
@@ -5,7 +5,9 @@ COLORS='-sb #000000 -sf #ffffff -nb #ffffff -nf #000000'
 DMENU_CMD="dmenu -p Execute: -i -b -fn $FONT $COLORS"
 
 DMENU_ITEMS="firefox:firefox_unsec:telegram:keepassxc"
-DMENU_ITEMS+=":android-file-transfer:qmapshack"
+DMENU_ITEMS+=":audacious"
+DMENU_ITEMS+=":qmapshack:josm"
+DMENU_ITEMS+=":android-file-transfer"
 
 case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
     'firefox')
@@ -20,11 +22,17 @@ case $(echo $DMENU_ITEMS | tr ':' '\n' | $DMENU_CMD) in
     'keepassxc')
         /usr/local/bin/keepassxc &
         ;;
-    'android-file-transfer')
-        /usr/local/bin/android-file-transfer &
+    'audacious')
+        /usr/bin/audacious &
         ;;
     'qmapshack')
          LC_MESSAGES="ru_RU.utf8" /usr/local/bin/qmapshack &
+        ;;
+    'josm')
+        /usr/bin/java -jar /home/drag0n/bin/josm/josm-tested.jar &
+        ;;
+    'android-file-transfer')
+        /usr/local/bin/android-file-transfer &
         ;;
     *)
         exit 1
