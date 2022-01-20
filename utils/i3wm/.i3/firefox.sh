8182cd8c39c400bbc1aa061c6cd6c5595c99a914
eugeneandrienko <evg.andrienko@gmail.com>
2022-01-20 20:42:17 +0300

    Disable firejail
    Firefox, after update, could not open links from command line.

--- utils/i3wm/.i3/firefox.sh
+++ utils/i3wm/.i3/firefox.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env sh
 
-# Remove nogroups from /etc/firejail/firefox-common.profile if no sound in Firefox
 if [ -f ~/.gentoo ]; then
-    if [ -x /usr/bin/firejail ] &&
-        [ -x /usr/bin/apulse ] &&
-        [ -x /usr/bin/firefox-bin ] &&
-        [ -r /etc/firejail/firefox.profile ]; then
-        firejail --profile=/etc/firejail/firefox.profile \
-        --whitelist=~/downloads/ \
+    if [ -x /usr/bin/apulse ] &&
+       [ -x /usr/bin/firefox-bin ]; then
         /usr/bin/apulse /usr/bin/firefox-bin &
     fi
 elif [ -f ~/.freebsd-thinkpad ]; then
