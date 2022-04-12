a78808b9d0a485b6cc503fc1cbda40ba764108a7
eugeneandrienko <evg.andrienko@gmail.com>
2022-04-12 22:49:07 +0300

    Add ssh key to store_sensitive_data.sh script

--- store_sensitive_data.sh
+++ store_sensitive_data.sh
@@ -5,6 +5,7 @@ gpg --export-secret-key -a EB6A4FC927DE468CAD383BA8713D984F56881209 > priv.key
 
 tar cf sensitive_data.tar ./pub.key ./priv.key
 tar rf sensitive_data.tar -C ~/secure .netrc .openweathermap
+tar rf sensitive_data.tar -C ~/.ssh id_rsa id_rsa.pub
 
 gzip --keep -9 sensitive_data.tar
 shred -vuz pub.key priv.key sensitive_data.tar
