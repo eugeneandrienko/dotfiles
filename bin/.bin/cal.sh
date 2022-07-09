80fc7f01c45400671db52b2a1967011416182e40
eugeneandrienko <evg.andrienko@gmail.com>
2022-07-10 00:33:18 +0300

    Fix cal viewing for Termux

--- bin/.bin/cal.sh
+++ bin/.bin/cal.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-if [ -f ~/.gentoo ] || [ -d ~/.termux ]; then
+if [ -f ~/.gentoo ]; then
     cal -n 3
+elif [ -d ~/.termux ]; then
+    cal -n 1
 elif [ -f ~/.debian ]; then
     ncal -3Mb
 else
