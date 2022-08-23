19190c474cc74f00c0c27d819504181fc5fb5f68
eugeneandrienko <evg.andrienko@gmail.com>
2022-08-24 01:14:09 +0300

    Use special script to mute sound on all my machines

---
+++ utils/i3wm/.i3/mute.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ -f ~/.zalman ]; then
+    amixer -c 0 -- sset Master toggle 1>/dev/null 2>&1;
+elif [ -f ~/.thinkpad ]; then
+    amixer -c 0 -- sset Master toggle 1>/dev/null 2>&1;
+    MASTER_STATE=$(amixer -c 0 -- sget Master | grep 'Mono:' | sed -r 's/.*\[(o[nf]f?)\]/\1/g')
+    HEADPHONE_LEVEL=$(amixer -c 0 -- sget Headphone | grep 'Front Left:' | sed -r 's/.*\[([0-9]+)%\].*/\1/g')
+    SPEAKER_LEVEL=$(amixer -c 0 -- sget Speaker | grep 'Front Left:' | sed -r 's/.*\[([0-9]+)%\].*/\1/g')
+    # Headphone will be muted automatically, if Master already muted:
+    if [ "$HEADPHONE_LEVEL" -ne "0" ] && [ "$MASTER_STATE" == "on" ]; then
+        amixer -c 0 -- sset Headphone toggle 1>/dev/null 2>&1;
+    # Speaker will be muted automatically, if Master already muted:
+    elif [ "$SPEAKER_LEVEL" -ne "0" ] && [ "$MASTER_STATE" == "on" ]; then
+        amixer -c 0 -- sset Speaker toggle 1>/dev/null 2>&1;
+    fi
+fi
+
