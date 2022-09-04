8b372589d6d2a6e56ae0b337359f93e503955e7e
eugeneandrienko <evg.andrienko@gmail.com>
2022-09-04 18:08:27 +0300

    Change auctex config for new AuCTeX plugin

--- utils/emacs/.emacs.d/rc-auctex.el
+++ utils/emacs/.emacs.d/rc-auctex.el
@@ -1,8 +1,5 @@
 (require 'tex-site)
-(require 'auctex-latexmk)
-(auctex-latexmk-setup)
 (setq TeX-PDF-mode t)
-(setq auctex-latexmk-inherit-TeX-PDF-mode t)
 
 
 (add-hook 'LaTeX-mode-hook '(lambda () (set-fill-column 80)))
@@ -17,26 +14,19 @@
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 ;;activate forward/reverse search
-(TeX-source-correlate-mode)
+(setq TeX-source-correlate-mode t)
+(setq TeX-source-correlate-method 'synctex)
 ;;setup Zathura as default document viewer
-(add-to-list 'TeX-view-program-list '("Zathura"
+(setq TeX-view-program-list '(("Zathura"
 				      ("zathura %o"
 				       (mode-io-correlate " --synctex-forward %n:0:%b -x \"emacsclient +%{line} %{input}\" "))
-				      "zathura"))
-(add-to-list 'TeX-view-program-selection '(output-pdf "Zathura"))
+				      "zathura")))
+(setq TeX-view-program-selection '((output-pdf "Zathura")))
 (setq LaTeX-command "latex")
 ;use pdflatex unstead of latex
 (setq TeX-PDF-mode t)
 ;enabled RefTeX
 (setq reftex-plug-into-AUCTeX t)
-(custom-set-faces
- '(font-latex-sectioning-0-face ((t (:inherit font-latex-sectioning-1-face :foreground "OliveDrab" :height 1.1))))
- '(font-latex-sectioning-1-face ((t (:inherit font-latex-sectioning-2-face :foreground "OliveDrab" :height 1.1))))
- '(font-latex-sectioning-2-face ((t (:inherit font-latex-sectioning-3-face :foreground "OliveDrab" :height 1.1))))
- '(font-latex-sectioning-3-face ((t (:inherit font-latex-sectioning-4-face :foreground "OliveDrab" :height 1.1))))
- '(font-latex-sectioning-4-face ((t (:inherit font-latex-sectioning-5-face :foreground "OliveDrab" :height 1.1))))
- '(font-latex-sectioning-5-face ((((class color) (background dark)) (:inherit variable-pitch :foreground "OliveDrab" :weight bold))))
- '(font-latex-string-face  ((t (:inherit font-lock-keyword-face :foreground "OliveDrab" :height 1.0)))))
 
 ;;templates for LaTeX
 (defun template-latex-report()
