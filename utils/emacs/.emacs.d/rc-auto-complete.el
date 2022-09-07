83de7e4c216ee9a1a18995542651e7542b66012b
eugeneandrienko <evg.andrienko@gmail.com>
2022-09-08 01:47:01 +0300

    Disable auto-complete for java-mode

--- utils/emacs/.emacs.d/rc-auto-complete.el
+++ utils/emacs/.emacs.d/rc-auto-complete.el
@@ -2,12 +2,17 @@
   :ensure auto-complete
   :config
   (progn (ac-config-default)
-	 (ac-flyspell-workaround))
+	 (ac-flyspell-workaround)
+         (defun auto-complete-mode-maybe ()
+           "No maybe for java-mode"
+           (unless (eq major-mode 'java-mode)
+             (auto-complete-mode 1))))
   :hook
   ((c-mode-common-hook . (lambda ()
 			   (add-to-list 'ac-omni-completion-sources
 					(cons "\\." '(ac-source-semantic)))
 			   (add-to-list 'ac-omni-completion-sources
 					(cons "->" '(ac-source-semantic)))
-			   (setq ac-sources '(ac-source-semantic))))))
-
+			   (setq ac-sources '(ac-source-semantic)))))
+  :custom
+  (ac-ignore-case 'smart' "Ignore case if string not include upper characters"))
