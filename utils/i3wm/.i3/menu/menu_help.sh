2fa19ae150785b58a77362747952e87dcc61c3fa
eugeneandrienko <evg.andrienko@gmail.com>
2023-01-14 18:39:03 +0300

    Add reboot menu
    Signed-off-by: eugeneandrienko <evg.andrienko@gmail.com>

--- utils/i3wm/.i3/menu/menu_help.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-notify-send -u low -t 800 "Help:" "d - programs\ns - system menu"
-
--- utils/i3wm/.i3/menu/menu_system.sh
+++ utils/i3wm/.i3/menu/menu_system.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env zsh
 
+source ~/.bin/get_machine_id.sh
+
 FONT="'-xos4-terminus-*-*-*-*-14-*-*-*-*-*-*-u'"
 COLORS="-sb '#000000' -sf '#ffffff' -nb '#ffffff' -nf '#000000'"
 DMENU_CMD="dmenu -i -b -fn $FONT $COLORS"
 
-MODES_ITEMS="display mode:redshift"
+MODES_ITEMS="display mode:redshift:reboot"
 case $(echo $MODES_ITEMS | tr ':' '\n' | eval "$DMENU_CMD -p Select:") in
     'display mode')
         case $(echo "normal:threatre" | tr ':' '\n' | eval "$DMENU_CMD -p '>'") in
@@ -28,8 +30,29 @@ case $(echo $MODES_ITEMS | tr ':' '\n' | eval "$DMENU_CMD -p Select:") in
                 ;;
         esac
         ;;
+    'reboot')
+        export SUDO_ASKPASS="/usr/bin/x11-ssh-askpass"
+        case "$MACHINE_HW" in
+            "zalman")
+                REBOOT_ITEMS="windows:linux"
+                ;;
+            "thinkpad")
+                REBOOT_ITEMS="linux"
+                ;;
+        esac
+        case $(echo $REBOOT_ITEMS | tr ':' '\n' | eval "$DMENU_CMD -p '>'") in
+            'windows')
+                sudo -A touch /root/REBOOT.reboot
+                sudo -A grub-reboot 'Windows 10 (on /dev/sda1)'
+                i3-msg exit
+                ;;
+            'linux')
+                sudo -A touch /root/REBOOT.reboot
+                i3-msg exit
+                ;;
+        esac
+        ;;
     *)
         exit 2
         ;;
 esac
-
