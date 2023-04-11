c846a648c10171245d36838b77b8bf4feeafb3fe
eugeneandrienko <evg.andrienko@gmail.com>
2023-04-11 21:57:02 +0300

    Change path to rss2email files

--- bin/.bin/get_rss.sh
+++ bin/.bin/get_rss.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/usr/bin/r2e -c ~/rsync/filez/rss/rss2email.cfg -d ~/rsync/filez/rss/rss2email.json run > /dev/null 2>&1
+/usr/bin/r2e -c ~/rsync/mail/rss2email.cfg -d ~/rsync/mail/rss2email.json run > /dev/null 2>&1
 cd ~/rsync/mail/rss-inbox
 find cur new -type f | while read FILENAME; do
     /usr/bin/maildrop < "$FILENAME"
