1ec95d52e133c128fd57247d8825288881b81446
eugeneandrienko <evg.andrienko@gmail.com>
2023-05-29 12:09:40 +0300

    Update RSS feeds only on main machine

--- bin/.bin/get_rss.sh
+++ bin/.bin/get_rss.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-/usr/bin/r2e -c ~/rsync/mail/rss2email.cfg -d ~/rsync/mail/rss2email.json run > /dev/null 2>&1
-cd ~/rsync/mail/rss-inbox
-find cur new -type f | while read FILENAME; do
-    /usr/bin/maildrop < "$FILENAME"
-    rm -f "$FILENAME"
-done
+source ~/.bin/get_machine_id.sh
+
+if [ "$MACHINE_HW" = "zalman" ]; then
+    /usr/bin/r2e -c ~/rsync/mail/rss2email.cfg -d ~/rsync/mail/rss2email.json run > /dev/null 2>&1
+    cd ~/rsync/mail/rss-inbox
+    find cur new -type f | while read FILENAME; do
+        /usr/bin/maildrop < "$FILENAME"
+        rm -f "$FILENAME"
+    done
+fi
 
