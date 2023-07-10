6769a9f5c76a8fd24b228bb73e3f474fa4cccff8
eugeneandrienko <evg.andrienko@gmail.com>
2023-07-11 00:19:32 +0300

    Wait for emacsclient when call editor from mutt and git

--- bin/.bin/emacs.sh
+++ bin/.bin/emacs.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-function emacs() {
+function emacsfunc() {
+    PARENT_PROGRAM=$(ps -o comm= $PPID)
+    EMACS_CMD="emacsclient"
     emacsclient -n -e "(> (length (frame-list)) 1)" | grep -q t
+
     if [ "$?" -eq "1" ]; then
-        emacsclient -c -n -a "" "$@"
-    else
-        emacsclient -n -a "" "$@"
+        # No Emacs frames — need to create a new one:
+        EMACS_CMD="$EMACS_CMD -c"
     fi
+    if [[ ! "mutt git" =~ "$PARENT_PROGRAM" ]]; then
+        # Shouldn't wait for buffer in Emacs — return immediately:
+        EMACS_CMD="$EMACS_CMD -n"
+    fi
+    # No alternate editor — start daemon if it is not running:
+    EMACS_CMD="$EMACS_CMD -a \"\""
+    eval "$EMACS_CMD $@"
 }
 
-emacs "$@"
+emacsfunc "$@"
+
