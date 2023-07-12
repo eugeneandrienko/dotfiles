14166a25c015791fbf3f0657837a66684d736e48
eugeneandrienko <evg.andrienko@gmail.com>
2023-07-13 00:43:13 +0300

    Make lockscreen nicer

--- utils/i3wm/.i3/scripts/lock.sh
+++ utils/i3wm/.i3/scripts/lock.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 
-i3lock -be -c 000000
+SCROT=/tmp/i3lock.png
+
+scrot -i "$SCROT"
+convert "$SCROT" -scale 10% -scale 1000% "$SCROT"
+i3lock -fe -c 000000 -i "$SCROT"
 sleep 5
 xset dpms force off
+rm -f "$SCROT" /tmp/i3lock_*.png
 
