df308537130fb8d720d7715201fb41f61639626d
eugeneandrienko <evg.andrienko@gmail.com>
2023-07-30 22:59:17 +0300

    Nicer lock screen

--- utils/i3wm/.i3/scripts/lock.sh
+++ utils/i3wm/.i3/scripts/lock.sh
@@ -2,7 +2,8 @@
 
 SCROT=/tmp/i3lock.png
 
-scrot -i "$SCROT"
+rm -f "$SCROT" /tmp/i3lock_*.png
+scrot -moipz -F "$SCROT"
 convert "$SCROT" -scale 10% -scale 1000% "$SCROT"
 i3lock -fe -c 000000 -i "$SCROT"
 sleep 5
