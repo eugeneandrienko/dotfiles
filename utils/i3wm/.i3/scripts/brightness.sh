392a9e377567e0f8534fbd66f4ca60430d9111e0
eugeneandrienko <evg.andrienko@gmail.com>
2023-11-18 18:43:21 +0300

    Show brightness value on Thinkpad.
    Closes #6

--- utils/i3wm/.i3/scripts/brightness.sh
+++ utils/i3wm/.i3/scripts/brightness.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 BRIGHTNESS="/sys/class/backlight/intel_backlight/brightness"
+BRIGHTNESS_MAX="/sys/class/backlight/intel_backlight/max_brightness"
 CURRENT_VALUE=$(cat $BRIGHTNESS)
+MAX_VALUE=$(cat $BRIGHTNESS_MAX)
+
 if [ "$1" = "up" ]; then
     OPERATOR="+"
 elif [ "$1" = "down" ]; then
@@ -10,6 +13,8 @@ else
     exit 1
 fi
 NEW_VALUE=$(echo "$CURRENT_VALUE""$OPERATOR"100 | bc)
+PERCENTAGE=$(printf %0.f $(echo "$NEW_VALUE"/"$MAX_VALUE"*100 | bc -l))
 
 echo "$NEW_VALUE" | sudo tee "$BRIGHTNESS" > /dev/null
+dunstify -h string:x-dunst-stack-tag:brightness -h int:value:"$PERCENTAGE" -t 1200 "Brightness:"
 
