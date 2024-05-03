274a0bb6fa8698f02cabc28bc22787231ed80a29
eugeneandrienko <evg.andrienko@gmail.com>
2024-05-04 00:19:03 +0300

    Add settings for Firefox to noauto/

---
+++ noauto/user.js
@@ -0,0 +1,285 @@
+/* All these settings were taken from here:
+ *
+ * https://github.com/yokoffing/Betterfox
+ */
+
+/*
+ * Remove annoyances:
+ */
+
+// PREF: Mozilla VPN
+// [1] https://github.com/yokoffing/Betterfox/issues/169
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+
+// PREF: disable about:addons' Recommendations pane (uses Google Analytics)
+user_pref("extensions.getAddons.showPane", false); // HIDDEN
+
+// PREF: disable recommendations in about:addons' Extensions and Themes panes
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+
+// PREF: disable Firefox from asking to set as the default browser
+// [1] https://github.com/yokoffing/Betterfox/issues/166
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+// PREF: disable Extension Recommendations (CFR: "Contextual Feature Recommender")
+// [1] https://support.mozilla.org/en-US/kb/extension-recommendations
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+
+// PREF: hide "More from Mozilla" in Settings
+user_pref("browser.preferences.moreFromMozilla", false);
+
+// PREF: enable Firefox to use userChome, userContent, etc.
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// PREF: add compact mode back to options
+user_pref("browser.compactmode.show", true);
+
+// PREF: remove fullscreen delay
+user_pref("full-screen-api.transition-duration.enter", "0 0"); // default=200 200
+user_pref("full-screen-api.transition-duration.leave", "0 0"); // default=200 200
+
+// PREF: disable fullscreen notice
+user_pref("full-screen-api.warning.delay", -1); // default=500
+user_pref("full-screen-api.warning.timeout", 0); // default=3000
+
+// PREF: minimize URL bar suggestions (bookmarks, history, open tabs)
+user_pref("browser.urlbar.suggest.engines", false);
+
+// PREF: Disable built-in Pocket extension
+user_pref("extensions.pocket.enabled", false);
+
+// PREF: always ask how to handle new mimetypes [FF101+]
+// Enforce user interaction for greater security.
+// [SETTING] General>Files and Applications>Applications>What should Firefox do with other files?
+// false=Save files
+// true=Ask whether to open or save files
+user_pref("browser.download.always_ask_before_handling_new_types", true);
+
+// PREF: autohide the downloads button
+user_pref("browser.download.autohideButton", true);
+
+// PREF: restore "View image info" on right-click
+user_pref("browser.menu.showViewImageInfo", true);
+
+// PREF: show all matches in Findbar
+user_pref("findbar.highlightAll", true);
+
+// PREF: limit the number of bookmark backups Firefox keeps
+user_pref("browser.bookmarks.max_backups", 3); // default=15
+
+// PREF: wrap long lines of text when using source / debugger
+user_pref("view_source.wrap_long_lines", true);
+user_pref("devtools.debugger.ui.editor-wrapping", true);
+
+
+/*
+ * Provide sensible security and privacy
+ */
+
+// PREF: disable UITour backend
+// This way, there is no chance that a remote page can use it.
+user_pref("browser.uitour.enabled", false);
+
+// PREF: display advanced information on Insecure Connection warning pages
+// [TEST] https://expired.badssl.com/
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+
+// PREF: prevent media cache from writing to disk in Private Browsing
+// [NOTE] MSE (Media Source Extensions) are already stored in-memory in PB
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
+
+// PREF: remove temp files opened with an external application
+// [1] https://bugzilla.mozilla.org/302433
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+
+// PREF: disable page thumbnails capturing
+// Page thumbnails are only used in chrome/privileged contexts.
+user_pref("browser.pagethumbnails.capturing_disabled", true);
+
+// PREF: set History section to show all options
+// Settings>Privacy>History>Use custom settings for history
+// [INFOGRAPHIC] https://bugzilla.mozilla.org/show_bug.cgi?id=1765533#c1
+user_pref("privacy.history.custom", true);
+
+// PREF: disable Firefox Suggest
+// [1] https://github.com/arkenfox/user.js/issues/1257
+//user_pref("browser.urlbar.quicksuggest.enabled", false); // controls whether the UI is shown
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+
+// PREF: enforce Punycode for Internationalized Domain Names to eliminate possible spoofing
+// Firefox has some protections, but it is better to be safe than sorry.
+// [!] Might be undesirable for non-latin alphabet users since legitimate IDN's are also punycoded.
+// [EXAMPLE] https://www.techspot.com/news/100555-malvertising-attack-uses-punycode-character-spread-malware-through.html
+// [TEST] https://www.xn--80ak6aa92e.com/ (www.apple.com)
+// [1] https://wiki.mozilla.org/IDN_Display_Algorithm
+// [2] https://en.wikipedia.org/wiki/IDN_homograph_attack
+// [3] CVE-2017-5383: https://www.mozilla.org/security/advisories/mfsa2017-02/
+// [4] https://www.xudongz.com/blog/2017/idn-phishing/
+user_pref("network.IDN_show_punycode", true);
+
+// PREF: disable capturing credentials in private browsing
+user_pref("signon.privateBrowsingCapture.enabled", false);
+
+// PREF: disable Firefox built-in password generator
+// Create passwords with random characters and numbers.
+// [NOTE] Doesn't work with Lockwise disabled!
+// [1] https://wiki.mozilla.org/Toolkit:Password_Manager/Password_Generation
+user_pref("signon.generation.enabled", false);
+
+// PREF: disable websites autocomplete
+// Don't let sites dictate use of saved logins and passwords.
+user_pref("signon.storeWhenAutocompleteOff", false);
+
+// PREF: prevent password truncation when submitting form data
+// [1] https://www.ghacks.net/2020/05/18/firefox-77-wont-truncate-text-exceeding-max-length-to-address-password-pasting-issues/
+user_pref("editor.truncate_user_pastes", false);
+
+// PREF: disallow PDFs to load javascript
+// https://www.reddit.com/r/uBlockOrigin/comments/mulc86/firefox_88_now_supports_javascript_in_pdf_files/
+user_pref("pdfjs.enableScripting", false);
+
+// PREF: enable FTP protocol
+// Firefox redirects any attempt to load a FTP resource.
+// to the default search engine if the FTP protocol is disabled.
+// [1] https://www.ghacks.net/2018/02/20/firefox-60-with-new-preference-to-disable-ftp/
+user_pref("network.ftp.enabled", true);
+
+// PREF: disable SB checks for downloads (remote)
+// To verify the safety of certain executable files, Firefox may submit some information about the
+// file, including the name, origin, size and a cryptographic hash of the contents, to the Google
+// Safe Browsing service which helps Firefox determine whether or not the file should be blocked.
+// [NOTE] If you do not understand the consequences, override this.
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+// PREF: disable search engine updates (e.g. OpenSearch)
+// [NOTE] This does not affect Mozilla's built-in or Web Extension search engines.
+user_pref("browser.search.update", false);
+
+// PREF: disable new data submission [FF41+]
+// If disabled, no policy is shown or upload takes place, ever.
+// [1] https://bugzilla.mozilla.org/1195552
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+
+// PREF: disable Health Reports
+// [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to send technical data.
+user_pref("datareporting.healthreport.uploadEnabled", false);
+
+// PREF: disable telemetry
+// - If "unified" is false then "enabled" controls the telemetry module
+// - If "unified" is true then "enabled" only controls whether to record extended data
+// [NOTE] "toolkit.telemetry.enabled" is now LOCKED to reflect prerelease (true) or release builds (false) [2]
+// [1] https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/telemetry/internals/preferences.html
+// [2] https://medium.com/georg-fritzsche/data-preference-changes-in-firefox-58-2d5df9c428b5 ***/
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false); // see [NOTE]
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false); // [FF57+] Background Hang Reporter
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+
+// PREF: disable Telemetry Coverage
+// [1] https://blog.mozilla.org/data/2018/08/20/effectively-measuring-search-in-firefox/
+user_pref("toolkit.telemetry.coverage.opt-out", true); // [HIDDEN PREF]
+user_pref("toolkit.coverage.opt-out", true); // [FF64+] [HIDDEN PREF]
+user_pref("toolkit.coverage.endpoint.base", "");
+
+// PREF: disable PingCentre telemetry (used in several System Add-ons) [FF57+]
+// Currently blocked by 'datareporting.healthreport.uploadEnabled'
+user_pref("browser.ping-centre.telemetry", false);
+
+// PREF: disable Firefox Home (Activity Stream) telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+// PREF: disable Studies
+// [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to install and run studies
+user_pref("app.shield.optoutstudies.enabled", false);
+
+// PREF: disable Normandy/Shield [FF60+]
+// Shield is an telemetry system (including Heartbeat) that can also push and test "recipes".
+// [1] https://mozilla.github.io/normandy/
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+
+// PREF: disable crash reports
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+
+// PREF: enforce no submission of backlogged crash reports
+// [SETTING] Privacy & Security>Firefox Data Collection & Use>Allow Firefox to send backlogged crash reports
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+
+// PREF: disallow websites to ask you for your location
+user_pref("permissions.default.geo", 0);
+
+// PREF: disallow websites to ask you to receive site notifications
+user_pref("permissions.default.desktop-notification", 0);
+
+
+/*
+ * Speed up browsing
+ */
+
+// PREF: compression level for cached JavaScript bytecode [FF102+]
+// [1] https://github.com/yokoffing/Betterfox/issues/247
+// 0 = do not compress (default)
+// 1 = minimal compression
+// 9 = maximal compression
+user_pref("browser.cache.jsbc_compression_level", 3);
+
+// PREF: image cache
+user_pref("image.mem.decode_bytes_at_a_time", 32768); // default=16384; alt=65536; chunk size for calls to the image decoders
+
+// PREF: increase the absolute number of HTTP connections
+// [1] https://kb.mozillazine.org/Network.http.max-connections
+// [2] https://kb.mozillazine.org/Network.http.max-persistent-connections-per-server
+// [3] https://www.reddit.com/r/firefox/comments/11m2yuh/how_do_i_make_firefox_use_more_of_my_900_megabit/jbfmru6/
+user_pref("network.http.max-connections", 1800); // default=900
+user_pref("network.http.max-persistent-connections-per-server", 10); // default=6; download connections; anything above 10 is excessive
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5); // default=3
+
+// PREF: DNS prefetching <link rel="dns-prefetch">
+// Used for cross-origin connections to provide small performance improvements.
+// Disable DNS prefetching to prevent Firefox from proactively resolving
+// hostnames for other domains linked on a page. This may eliminate
+// unnecessary DNS lookups, but can increase latency when following external links.
+// [1] https://bitsup.blogspot.com/2008/11/dns-prefetching-for-firefox.html
+// [2] https://css-tricks.com/prefetching-preloading-prebrowsing/#dns-prefetching
+// [3] https://www.keycdn.com/blog/resource-hints#2-dns-prefetching
+// [4] http://www.mecs-press.org/ijieeb/ijieeb-v7-n5/IJIEEB-V7-N5-2.pdf
+user_pref("network.dns.disablePrefetch", true);
+
+// PREF: link prefetching <link rel="prefetch">
+// Pre-populates the HTTP cache by prefetching same-site future navigation
+// resources or subresources used on those pages.
+// Enabling link prefetching allows Firefox to preload pages tagged as important.
+// The browser prefetches links with the prefetch-link tag, fetching resources
+// likely needed for the next navigation at low priority. When clicking a link
+// or loading a new page, prefetching stops and discards hints. Prefetching
+// downloads resources without executing them.
+// [NOTE] Since link prefetch uses the HTTP cache, it has a number of issues
+// with document prefetches, such as being potentially blocked by Cache-Control headers
+// (e.g. cache partitioning).
+// [1] https://developer.mozilla.org/en-US/docs/Glossary/Prefetch
+// [2] http://www.mecs-press.org/ijieeb/ijieeb-v7-n5/IJIEEB-V7-N5-2.pdf
+// [3] https://timkadlec.com/remembers/2020-06-17-prefetching-at-this-age/
+// [4] https://3perf.com/blog/link-rels/#prefetch
+// [5] https://developer.mozilla.org/docs/Web/HTTP/Link_prefetching_FAQ
+user_pref("network.prefetch-next", false);
+
+// PREF: Network Predictor (NP)
+// When enabled, it trains and uses Firefox's algorithm to preload page resource
+// by tracking past page resources. It uses a local file (history) of needed images,
+// scripts, etc. to request them preemptively when navigating.
+// [NOTE] By default, it only preconnects, doing DNS, TCP, and SSL handshakes.
+// No data sends until clicking. With "network.predictor.enable-prefetch" enabled,
+// it also performs prefetches.
+// [1] https://wiki.mozilla.org/Privacy/Reviews/Necko
+// [2] https://www.ghacks.net/2014/05/11/seer-disable-firefox/
+// [3] https://github.com/dillbyrne/random-agent-spoofer/issues/238#issuecomment-110214518
+// [4] https://www.igvita.com/posa/high-performance-networking-in-google-chrome/#predictor
+user_pref("network.predictor.enabled", false);
