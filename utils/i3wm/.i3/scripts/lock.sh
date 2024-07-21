2ea2092eba6570ad9afa493885c93a65740330f6
eugeneandrienko <evg.andrienko@gmail.com>
2024-07-21 15:45:05 +0300

    Do not disable screen after locking on ThinkPad

--- utils/i3wm/.i3/scripts/lock.sh
+++ utils/i3wm/.i3/scripts/lock.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+source ~/.bin/get_machine_id.sh
+
 SCROT=/tmp/i3lock.png
 
 rm -f "$SCROT" /tmp/i3lock_*.png
@@ -7,6 +9,8 @@ scrot -moipz -F "$SCROT"
 convert "$SCROT" -scale 10% -scale 1000% "$SCROT"
 i3lock -fe -c 000000 -i "$SCROT"
 sleep 5
-xset dpms force off
+if [ "$MACHINE_HW" != "thinkpad" ]; then
+    xset dpms force off
+fi
 rm -f "$SCROT" /tmp/i3lock_*.png
 
