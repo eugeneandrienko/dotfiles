2f48fa16aeafb9a4658f1380849f47f8fe07ef37
eugeneandrienko <evg.andrienko@gmail.com>
2024-08-09 03:22:31 +0300

    Remove unnecessary ":ensure t"

--- utils/emacs/.emacs.d/rc-pass.el
+++ utils/emacs/.emacs.d/rc-pass.el
@@ -1,12 +1,9 @@
-(use-package password-store
-  :ensure t)
+(use-package password-store)
 
 (use-package password-store-otp
-  :ensure t
   :after password-store)
 
 (use-package pass
-  :ensure t
   :after password-store-otp
   :custom
   (pass-username-fallback-on-filename t))
