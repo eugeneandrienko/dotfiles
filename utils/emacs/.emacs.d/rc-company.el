442ceee44f599c6fdc4f5d4fe892abfd368d5503
eugeneandrienko <evg.andrienko@gmail.com>
2024-10-20 18:48:49 +0300

    Configured autocompletion via company-mode for C sources

--- utils/emacs/.emacs.d/rc-company.el
+++ utils/emacs/.emacs.d/rc-company.el
@@ -1,38 +1,39 @@
 (use-package company
   :delight
   :pin melpa
+  :bind
+  (:map company-active-map
+        ("TAB" . 'company-complete-common-or-cycle)
+        ("<tab>" . 'company-complete-common-or-cycle)
+        ("<backtab>" . (lambda ()
+                         (interactive)
+                         (company-complete-common-or-cycle -1))))
   :custom
-  (company-idle-delay 0.1 "Delay before show completions")
-  (company-minimum-prefix-length 2 "Characters to start completion")
+  (company-idle-delay 0.2 "Delay before show completions")
+  (company-minimum-prefix-length 3 "Characters to start completion")
   (company-tooltip-align-annotations t "Right align additional annotations")
+  (company-inhibit-inside-symbols t "Disable completion in the middle of string")
   (company-global-modes '(java-mode
                           c-mode
                           emacs-lisp-mode)
                         "Modes with enabled completion")
   (company-format-margin-function #'company-text-icons-margin "Use text-icons with completion results")
   (company-show-quick-access 'left "Show quick access hints")
-  (company-backends '(company-etags
-                      company-bbdb
-                      company-semantic
-                      company-capf
-                      company-files
-                      (company-dabbrev-code company-gtags company-keywords)
-                      company-oddmuse
-                      company-dabbrev)
+  (company-transformers '(delete-consecutive-dups company-sort-by-occurrence)
+                        "Filters for completion results")
+  :config
+  (global-company-mode))
+
+(use-package company-ctags
+  :delight
+  :pin melpa
+  :after company
+  :custom
+  (company-ctags-fuzzy-match-p t "Enable fuzzy matching")
+  (company-ctags-tags-file-name "TAGS" "ctags file name")
+  (company-ctags-extra-tags-files '("$HOME/git_repos/palm-sync-daemon/TAGS"))
+  (company-backends '((company-ctags company-capf company-semantic)
+                      company-keywords)
                     "Enabled backends")
   :config
-  (with-eval-after-load 'company
-                                        ; for CLI
-    (define-key company-active-map
-                (kbd "TAB")
-                #'company-complete-common-or-cycle)
-                                        ; for GUI
-    (define-key company-active-map
-                (kbd "<tab>")
-                #'company-complete-common-or-cycle)
-    (define-key company-active-map
-                (kbd "<backtab>")
-                (lambda ()
-                  (interactive)
-                  (company-complete-common-or-cycle -1))))
-  (global-company-mode))
+  (company-ctags-auto-setup))
