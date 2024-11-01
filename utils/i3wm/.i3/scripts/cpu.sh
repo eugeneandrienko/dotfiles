51a3397e9cffcb87fa28dfcc8d1ac358ff40a41b
eugeneandrienko <evg.andrienko@gmail.com>
2024-11-01 03:42:39 +0300

    Properly display load average on i3bar

--- utils/i3wm/.i3/scripts/cpu.sh
+++ utils/i3wm/.i3/scripts/cpu.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 if [ "$MACHINE_HW" = "thinkpad" ] && [ "$MACHINE_OS" = "freebsd" ]; then
-    CPU_LOAD="$(uptime | sed -r 's/.+ages: ([0-9.]+),.+/(\1 * 100)$/g' | bc)"
+    CPU_LOAD="$(uptime | sed -r 's/.+ages: ([0-9.]+),.+/\1/g')"
     CPU_TEMP="$(sysctl -n hw.acpi.thermal.tz0.temperature | sed 's/C//g')"
-    echo "🧠$CPU_LOAD% $CPU_TEMP°C"
-    echo "🧠$CPU_LOAD% $CPU_TEMP°C"
-    if [ "$CPU_LOAD" -ge "90" ] || [ 1 -eq "$(echo $CPU_TEMP '>' 80 | bc)" ]; then
+    echo "🧠$CPU_LOAD $CPU_TEMP°C"
+    echo "🧠$CPU_LOAD $CPU_TEMP°C"
+    if [ 1 -eq "$(echo $CPU_TEMP '>' 80 | bc)" ]; then
         echo "#DC322F"
     fi
 else
