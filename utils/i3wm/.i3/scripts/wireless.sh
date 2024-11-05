9479f141d8357da5bbfdfd25978afae066a5c0fd
eugeneandrienko <evg.andrienko@gmail.com>
2024-11-05 22:42:22 +0300

    Properly parse unknown wireless states, process "wifi down" state

--- utils/i3wm/.i3/scripts/wireless.sh
+++ utils/i3wm/.i3/scripts/wireless.sh
@@ -2,20 +2,28 @@
 
 if [ "$MACHINE_HW" = "thinkpad" ] && [ "$MACHINE_OS" = "freebsd" ]; then
     WPA_STATUS="$(wpa_cli status)"
-    WPA_STATE="$(echo $WPA_STATUS | grep '^wpa_state=' | sed -r 's/wpa_state=(.+)/\1/g')"
-    if [ "$WPA_STATE" = "DISCONNECTED" ]; then
-        echo ""
-    elif [ "$WPA_STATE" = "SCANNING" ]; then
-        echo "📶 scan"
-        echo "📶"
-        echo "#FF8000"
+    if [ "$?" -eq "0" ]; then
+        WPA_STATE="$(echo $WPA_STATUS | grep '^wpa_state=' | sed -r 's/wpa_state=(.+)/\1/g')"
     else
-        SSID="$(echo $WPA_STATUS | grep '^ssid=' | sed -r 's/ssid=(.+)/\1/g')"
-        IP_ADDRESS="$(echo $WPA_STATUS | grep '^ip_address=' | sed -r 's/ip_address=(.+)/\1/g')"
-        echo "$SSID ($IP_ADDRESS)"
-        echo "$SSID"
-        echo "#2AA198"
+        WPA_STATE="DOWN"
     fi
+    case "$WPA_STATE" in
+        "DISCONNECTED"|"DOWN")
+            echo ""
+            ;;
+        "SCANNING")
+            echo "📶 scan"
+            echo "📶"
+            echo "#FF8000"
+            ;;
+        *)
+            SSID="$(echo $WPA_STATUS | grep '^ssid=' | sed -r 's/ssid=(.+)/\1/g')"
+            IP_ADDRESS="$(echo $WPA_STATUS | grep '^ip_address=' | sed -r 's/ip_address=(.+)/\1/g')"
+            echo "$SSID ($IP_ADDRESS)"
+            echo "$SSID"
+            echo "#2AA198"
+            ;;
+    esac
 
     if [ "$BLOCK_BUTTON" -eq 1 ]; then
         i3-msg -q exec wpa_gui
