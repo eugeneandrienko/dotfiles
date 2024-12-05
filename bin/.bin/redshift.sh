32fa573beee9a396b736f5d23c9c5c642e5bd44a
eugeneandrienko <evg.andrienko@gmail.com>
2024-12-05 23:44:25 +0300

    Just kill existing redshift processes before startup new one

--- bin/.bin/redshift.sh
+++ bin/.bin/redshift.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
 
 ps aux | grep -v 'grep' | grep -q 'redshift -l'
-if [ "$?" -ne "0" ]; then
-    source ~/.bin/get_machine_id.sh
-    case "$MACHINE_OS" in
-        "gentoo")
-            redshift -l $(~/.bin/geocoords.sh) -m vidmode &
-            ;;
-        "freebsd")
-            redshift -l $(~/.bin/geocoords.sh) -m randr &
-            ;;
-    esac
+if [ "$?" -eq "0" ]; then
+    pkill -9 redshift
 fi
-
+source ~/.bin/get_machine_id.sh
+case "$MACHINE_OS" in
+    "gentoo")
+        redshift -l $(~/.bin/geocoords.sh) -m vidmode &
+        ;;
+    "freebsd")
+        redshift -l $(~/.bin/geocoords.sh) -m randr &
+        ;;
+esac
